c080e24905de03e0b119a9d6f035121c705506b4
claudiofilho87 <86245219+claudiofilho87@users.noreply.github.com>
2022-12-27 07:54:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,41 +6,6 @@ from time import sleep
 
 URL = "https://www.linkedin.com/jobs/search/?f_LF=f_AL&geoId=102257491&keywords=python%20developer&location=London%2C%20England%2C%20United%20Kingdom&redirect=false&position=1&pageNum=0"
 
-# ======================================================================================================================
-# # First way with google login
-# ======================================================================================================================
-
-# driver = uc.Chrome(use_subprocess=True)
-# driver.get(URL)
-#
-# main_page = driver.current_window_handle
-#
-# sleep(2)
-# button_login = driver.find_element(By.XPATH, "/html/body/div[1]/header/nav/div/a[2]")
-# button_login.click()
-# sleep(2)
-# google_login = driver.find_element(By.XPATH, "//*[@id='sign-in-with-google-button']")
-# google_login.click()
-# sleep(2)
-# driver.switch_to.window(driver.window_handles[-1])
-# email_login = driver.find_element(By.XPATH, '//*[@id="identifierId"]')
-# email_login.send_keys("claudiofilho1525@gmail.com")
-# button_next = driver.find_element(By.XPATH, '//*[@id="identifierNext"]/div/button/span')
-# button_next.click()
-# sleep(2)
-# password_login = driver.find_element(By.XPATH, '//*[@id="password"]/div[1]/div/div[1]/input')
-# password_login.send_keys("C2468101214c")
-# sleep(2)
-# button_finish = driver.find_element(By.XPATH, '//*[@id="passwordNext"]/div/button/span')
-# button_finish.click()
-# sleep(100)
-# ======================================================================================================================
-
-
-# ======================================================================================================================
-# # Second way with directly login
-# ======================================================================================================================
-
 chrome_driver_path = "C:\Downloads\chromedriver_win32\chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
@@ -51,10 +16,10 @@ button_login = driver.find_element(By.XPATH, "/html/body/div[1]/header/nav/div/a
 button_login.click()
 sleep(1)
 email_login = driver.find_element(By.XPATH, '//*[@id="username"]')
-email_login.send_keys("claudiofilho1525@gmail.com")
+email_login.send_keys("YOUR EMAIL")
 sleep(1)
 password_login = driver.find_element(By.XPATH, '//*[@id="password"]')
-password_login.send_keys("C2468101214")
+password_login.send_keys("YOUR PASSWORD")
 button_finish = driver.find_element(By.XPATH, '//*[@id="organic-div"]/form/div[3]/button')
 button_finish.click()
 # sleep(15)
